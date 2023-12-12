3f089bc329fc96f352478fc35e7058d39b34856b
cesario <cesarioneto7@gmail.com>
2023-12-12 10:51:28 -0300

    fixed bugs

--- app/gallery/admin.py
+++ app/gallery/admin.py
@@ -7,9 +7,11 @@ from utils.images import get_new_width_and_height
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
     list_display = 'title', 'owner', 'photo', 'visible',
+    search_fields = 'title',
+    list_filter = 'created_at', 'updated_at', 'visible',
 
     def display_photo(self, obj):
-        width, height = get_new_width_and_height(obj.photo.url)
+        width, height = get_new_width_and_height(obj.photo.path)
         return format_html(f'<img src="{obj.photo.url}" alt="" width="{width}" height="{height}">')
 
     readonly_fields = ['display_photo']
