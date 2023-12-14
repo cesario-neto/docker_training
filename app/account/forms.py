e00794cdd4add0af641554aa4a38ae1b15cf7061
cesario <cesarioneto7@gmail.com>
2023-12-14 10:55:15 -0300

    create forms for login and register

---
+++ app/account/forms.py
@@ -0,0 +1,63 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+from django.core.exceptions import ValidationError
+
+
+class RegisterForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control rounded-5',
+            'placeholder': 'Senha'})
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-control rounded-5',
+            'placeholder': 'Repita a senha'})
+
+    class Meta:
+        model = User
+        fields = [
+            'email',
+            'username',
+            'password1',
+            'password2',
+        ]
+        widgets = {
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control rounded-5',
+                'placeholder': 'Email',
+            }),
+            'username': forms.TextInput(attrs={
+                'class': 'form-control rounded-5',
+                'placeholder': 'Nome de Usuário',
+            }),
+        }
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'required': True,
+            'class': 'form-control rounded-5',
+            'placeholder': 'Email',
+        })
+    )
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'required': True,
+            'class': 'form-control rounded-5',
+            'placeholder': 'Senha',
+        })
+    )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if not User.objects.filter(email__exact=email).exists():
+            raise ValidationError(
+                'Email e/ou senha invalida',
+                code='invalid'
+            )
+
+        return email
